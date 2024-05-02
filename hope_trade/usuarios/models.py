0a7657f5489817def2b5733c569c9c8ecedd7c89
Valentin Ciamparella <ciamparellar@gmail.com>
2024-05-02 16:38:43 -0300

    Actualizo 5/2/2024
    me faltaban unos campos

--- hope_trade/usuarios/models.py
+++ hope_trade/usuarios/models.py
@@ -5,9 +5,14 @@ class intercambiador(Model.models):
     Email = models.EmailField(max_length=50,unique = True)
     Dni = models.CharField(min_length = 8,unique = True)
     Fnac = models.DateField
+    Clave = models.CharField(min_length = 6) 
+    Estado = models.BooleanField(default=False) #Aca tomo solo dos valores, activo o suspendido, si se elimina tbn se elimina de la BD
 
 class ayudante(Model.models):
     Email = models.EmailField(max_length=50,unique = True)
     Dni = models.CharField(min_length = 8,unique = True)
     Fnac = models.DateField
+    Clave = models.CharField(min_length = 6) 
+    Filial = ArrayField(models.CharField(max_length=50)) #chequear esta linea :) 
+    
 
